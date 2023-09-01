2064aada27e820503762427dc452edb722095dcd
T Mark <tmk1.tran@gmail.com>
2023-09-01 18:56:39 -0500

    added some tests, console.logs, and comments. new files are experimental, remove later

--- js/client.js
+++ js/client.js
@@ -1,26 +1,105 @@
-// or
-// $( function () {});
-
 console.log('Hello World');
-let studentCount = 0;
+let monthlyCosts = 0;
 
 $(onReady); // "new" way to set onReady function
 
 function onReady() {
-    // Wire up our click handlers
-   // Add Cohort Button
-   $('#addCohort').on('click', addCohort);
+   console.log( 'in onReady' );
+   // Wire up our click handlers
+   // An add Button
+   $('#addEmployee').on('click', addEmployee);
+   console.log( 'listening for addEmployee!' );
 
-   // Delete Cohort Button
-   $('.delete').on('click', deleteCohort);
+   // Delete Button
+   $('#deleteEmployee').on('click', deleteEmployee);
+   console.log( 'listening for deleteEmployee!' );
 
-   // Listen on #cohorts ul item, for clicks on children.
+   // Listen on #employees ul item, for clicks on children.
    // If an element (like a button) started the click event
-   // that had a class of '.delete', THEN call deleteCohort()
+   // that had a class of '.delete', THEN call deleteEmployee()
    // Why? Because delete buttons do NOT exist when onReady
    // first fires, they're added later via user actions
    // so we have to add our listener to something else on the
    // DOM that's available now and is an ancestor of the
-   // element we DO care about
-   $('#cohorts').on('click', '.delete', deleteCohort);
+   // element we DO care about (*FIX THIS LATER, UPDATE #EMPLOYEES CLASS*)
+   $('#employees').on('click', '.delete', deleteEmployee);
 }
+
+// A 'Submit' button should collect the form information,
+function addEmployee(event) {
+   console.log( 'in addEmployee' );
+}
+// store the information to calculate monthly costs,
+
+// append information to the DOM and
+
+// clear the input fields
+
+// Add a delete button
+function deleteEmployee(event) {
+   console.log( 'in deleteEmployee' );
+   // Update the global count
+   // Walk the DOM: Go from button (event.target) UP to the list-item (.cohort)
+   // then search down for the td that has the current cohort count (.current-count)
+   // then, grab the text value (number), convert it to a number, subtract
+   // that value from the global count, and update the DOM
+   const currentCount = $(event.target).closest('.cohort').find('.current-count').text();
+   studentCount -= Number(currentCount);
+   $('#totalStudents').text(studentCount);
+
+   // $(event.target).css('background-color', 'red');
+   $(event.target).parent().remove(); // removes parent only
+   $(event.target).closest('.employee').remove(); // searches ancestors (preferred)
+
+   // When there are more than 10 students, make it bold
+   if (studentCount < 10) {
+      $('#totalStudents').css('text-decoration', 'none');
+   } 
+}
+
+
+
+
+
+// ~~~~~~~~~Extra/ Notes Below~~~~~~~~~~~
+
+
+/*function addEmployee(event) {
+   console.log('in add ');
+   const employeeName = $('#employeeName').val();
+   const salaryCount = $('#salaryCount').val();
+   
+   // add event.disable here to prevent page loading when button clicked
+
+   console.log('in add', employeeName, salaryCount);
+} // take out later
+
+   // append to the ul*/
+  /* $('#cohorts').append(`<li class="cohort">
+      <table>
+         <tbody>
+            <tr>
+               <td><strong>Cohort Name:</strong></td>
+               <td>${cohortName}</td>
+            </tr>
+            <tr>
+               <td><strong># Students:</strong></td>
+               <td class="current-count">${cohortCount}</td>
+            </tr>
+         </tbody>
+      </table>
+      <button class="delete">Delete</button>
+   </li>`);
+   // Clear the fields
+   $('#cohortName').val('');
+   $('#cohortCount').val('');
+
+   // Update student count
+   studentCount += Number(cohortCount);
+   $('#totalStudents').text(studentCount);
+
+   // When there are more than 10 students, make it bold
+   if (studentCount > 10) {
+      $('#totalStudents').css('text-decoration', 'underline');
+   } 
+} */
---
+++ js/tryTHIS.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    // Initialize total monthly cost
+    var totalMonthlyCost = 0;
+
+    // Function to calculate and update total monthly cost
+    function updateTotalMonthlyCost() {
+        $("#totalStudents").text("$" + totalMonthlyCost.toFixed(2));
+        if (totalMonthlyCost > 20000) {
+            $("#totalStudents").addClass("high-cost");
+        } else {
+            $("#totalStudents").removeClass("high-cost");
+        }
+    }
+
+    // Event handler for submitting the form
+    $("#addEmployee").click(function(event) {
+        event.preventDefault();
+
+        // Get input values
+        var firstName = $("#firstName").val();
+        var lastName = $("#lastName").val();
+        var id = $("#id").val();
+        var title = $("#title").val();
+        var annualSalary = parseFloat($("#annualSalary").val());
+
+        // Calculate monthly cost
+        var monthlyCost = annualSalary / 12;
+
+        // Add employee to the list
+        var employeeInfo = firstName + " " + lastName + " (ID: " + id + ", Title: " + title + ")";
+        $("#cohorts").append("<li>" + employeeInfo + ": $" + monthlyCost.toFixed(2) + " per month</li>");
+
+        // Update total monthly cost
+        totalMonthlyCost += monthlyCost;
+        updateTotalMonthlyCost();
+
+        // Clear input fields
+        $("#firstName").val("");
+        $("#lastName").val("");
+        $("#id").val("");
+        $("#title").val("");
+        $("#annualSalary").val("");
+    });
+});
