557a31065276494a058d65cbced7f111096150a4
T Mark <tmk1.tran@gmail.com>
2023-09-01 22:10:02 -0500

    got monthly totals to reset to 0!

--- js/client.js
+++ js/client.js
@@ -30,8 +30,10 @@ function onReady() {
     const monthlyCost = annualSalary / 12;
     monthlyCosts += monthlyCost;
 
+    // inserted data-salary below to reference employee that was deleted, 
+    // and set value back to 0 in DOM
     $("#employees").append(`
-            <li class="employee">
+            <li class="employee" data-salary="${annualSalary}"> 
                 <table>
                     <tbody>
                         <tr>
@@ -65,20 +67,16 @@ function onReady() {
 
   // Delete Button
   $('#employees').on('click', '.delete', function () { // which elements inside #employees should trigger the event handler when clicked
-   console.log( 'you clicked delete!' );
+   
       const $employee = $(this).closest('.employee'); // find the closest ancestor element with the class 'employee'
-      
-      // Find the <td> element containing the text 'Salary:' within the employee element,
-      // then select the next sibling <td> element, which contains the actual salary text
-      const salaryText = $employee.find('td:contains("Salary:")').next().text();
-      
-      const salaryCount = parseFloat(salaryText.slice(1)); // // remove the dollar sign ('$') from the salary text and parse it as a float
+
+      const salaryCount = parseFloat($employee.data('salary')); // Retrieve the salary from the data-salary attribute
 
       const monthlyCost = salaryCount / 12; // calculate the monthly cost by dividing the annual salary by 12
 
       monthlyCosts -= monthlyCost; // subtract the monthly cost of the deleted employee from the total monthly costs
 
-      $employee.remove();
+      $employee.remove(); // remove the employee entry
 
       updateCosts();
   });
@@ -107,42 +105,16 @@ function onReady() {
 
 // ~~~~~~~~~Extra/ Notes Below~~~~~~~~~~~
 
-/*function addEmployee(event) {
-   console.log('in add ');
-   const employeeName = $('#employeeName').val();
-   const salaryCount = $('#salaryCount').val();
+
    
    // add event.disable here to prevent page loading when button clicked
 
-   console.log('in add', employeeName, salaryCount);
-} // take out later
 
-   // append to the ul*/
-/* $('#cohorts').append(`<li class="cohort">
-      <table>
-         <tbody>
-            <tr>
-               <td><strong>Cohort Name:</strong></td>
-               <td>${cohortName}</td>
-            </tr>
-            <tr>
-               <td><strong># Students:</strong></td>
-               <td class="current-count">${cohortCount}</td>
-            </tr>
-         </tbody>
-      </table>
-      <button class="delete">Delete</button>
-   </li>`);
-   // Clear the fields
-   $('#cohortName').val('');
-   $('#cohortCount').val('');
+
+
 
    // Update student count
-   studentCount += Number(cohortCount);
-   $('#totalStudents').text(studentCount);
+   // studentCount += Number(cohortCount);
+   // $('#totalStudents').text(studentCount);
 
-   // When there are more than 10 students, make it bold
-   if (studentCount > 10) {
-      $('#totalStudents').css('text-decoration', 'underline');
-   } 
-} */
+
